cf972f50ad0352f45223f7cd78cec2b818e21baa
valeninki <valenmortus@protonmail.com>
2025-10-01 20:49:39 +0300

    Configured wireguard.

---
+++ hosts/desktop/host/wireguard/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, config, ... }:
+
+{
+  networking = {
+    wireguard = {
+      interfaces = {
+        wg0 = {
+          ips = [ "10.0.0.3/32" ];
+	 listenPort = 51820;
+	 privateKeyFile = "/home/valentinus/.dots/hosts/thinkpad/host/wireguard/wireguard-keys/private";
+
+	 peers = [
+            {
+              publicKey = "0Js1Z2BB8GhikRes0vSk8Kr9CUYcFs6f46EymR5iyVE=";
+	     allowedIPs = [
+                "162.159.136.234/32"
+		"162.159.135.234/32"
+		"162.159.134.234/32"
+		"162.159.133.234/32"
+		"162.159.130.234/32"
+	     ];
+	     endpoint = "188.245.211.104:51820";
+	     persistentKeepalive = 25;
+	   }
+	 ];
+	};
+      };
+    };
+  };
+}
